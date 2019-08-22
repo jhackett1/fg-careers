dacfa535ba39d0e1dedf92725657c8824c7f4af1
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-08-22 14:47:34 +0100

    create new vacancy page app

--- vacancy/models.py
+++ vacancy/models.py
@@ -3,9 +3,14 @@ from django.db import models
 from wagtail.core.models import Page
 # from wagtail.admin.edit_handlers import FieldPanel
 
-class HomePage(Page):
+class VacancyPage(Page):
     template = "home/home_page.html"
-    max_count = 1
+
+    parent_page_type = [
+        "home.HomePage"
+    ]
+
+    
 
     # page_title = models.CharField(max_length=100, blank=True, null=False)
 
