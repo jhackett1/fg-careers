f04bcad0efd856d55c2ccdaf133bd73a7d744638
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-11 23:09:22 +0100

    thunk the settings

--- fgcareers/settings/production.py
+++ fgcareers/settings/production.py
@@ -1,24 +1,17 @@
-# from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals
 from .base import *
+import dj_database_url
+import os
 
-# import dj_database_url
-# import os
+env = os.environ.copy()
 
-# env = os.environ.copy()
-
-# SECRET_KEY = env['SECRET_KEY']
-
-# DATABASES['default'] = dj_database_url.config()
-
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO' , 'https')
-
-# ALLOWED_HOSTS = ['*']
-
+SECRET_KEY = env["SECRET_KEY"]
+DATABASES['default'] = dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO' , 'https')
+ALLOWED_HOSTS = ['*']
 DEBUG = False
 
 try:
     from .local import *
 except ImportError:
     pass
-
-
