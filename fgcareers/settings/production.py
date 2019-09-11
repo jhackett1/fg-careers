69ef43479a7a847a924832139447d74e93d7dcf5
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-11 16:48:03 +0100

    use heroku env settings

--- fgcareers/settings/production.py
+++ fgcareers/settings/production.py
@@ -1,5 +1,14 @@
 from .base import *
 
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO' , 'https')
+
+ALLOWED_HOSTS = ['*']
+
 DEBUG = False
 
 try:
