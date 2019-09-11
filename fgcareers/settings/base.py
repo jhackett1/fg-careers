d0b6bb3abae0aef4815703695e9e2f81045226ad
Jaye <joshua.hackett93@gmail.com>
2019-09-11 16:06:36 +0100

    postgres prep

--- fgcareers/settings/base.py
+++ fgcareers/settings/base.py
@@ -94,13 +94,19 @@ WSGI_APPLICATION = 'fgcareers.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backbends.postgresql_psycopg2',
+        'NAME': 'fgcareers_db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
