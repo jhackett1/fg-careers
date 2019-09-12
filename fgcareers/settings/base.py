dbf1f00148dfec8d75ca550634dea68fc027ea44
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-09-12 08:25:21 +0100

    unbork the thing

--- fgcareers/settings/base.py
+++ fgcareers/settings/base.py
@@ -5,10 +5,13 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 # Application definition
 INSTALLED_APPS = [
+    'storages',
+
     'home',
     'search',
     'vacancy',
     'streams',
+
     # Activate admin UI style guide
     'wagtail.contrib.styleguide',
     'wagtail.contrib.forms',
@@ -117,7 +120,28 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+
+
+# S3 setup
+env = os.environ.copy()
+
+AWS_STORAGE_BUCKET_NAME = env["AWS_STORAGE_BUCKET_NAME"]
+AWS_ACCESS_KEY_ID = env["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = env["AWS_SECRET_ACCESS_KEY"] 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Something to do with wagtail admin css: https://wagtail.io/blog/wagtail-heroku-2017/
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
 
 # Wagtail settings
 WAGTAIL_SITE_NAME = "fgcareers"
@@ -125,5 +149,3 @@ WAGTAIL_SITE_NAME = "fgcareers"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
-
-print(os.getenv("DJANGO_SETTINGS_MODULE"))
--- fgcareers/settings/production.py
+++ fgcareers/settings/production.py
@@ -11,7 +11,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARD_PROTO' , 'https')
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 
+
+
 try:
     from .local import *
 except ImportError:
     pass
+
